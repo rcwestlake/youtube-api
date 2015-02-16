5ee396ab61d7ffa2358c150b49e35d38560e9f29
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-16 11:24:14 -0700

    changes to js to fix link error

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
+//my API key AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E
+
 $(document).ready(function() {
 	console.log('js check');
 
-	//my API key AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E
-
+	
 	function onYouTubeApiLoad() {
    		// This API key is intended for use only in this lesson.
     	// See http://goo.gl/PdPA1 to get a key for your own applications.
     	gapi.client.setApiKey('AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E');
     }	
 
-	$(function() {
+    var getRequest = function(data) {
+    	var params = {
+    		part: 'snippet'
+    		key: 'AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E'
+    		q: "basketball"
+    	};
+    	url: 'https://www.googleapis.com/youtube/v3/search'
+
+    	$.getJSON(url, params, function(data) {
+    		console.log(data);
+    	});
+    }
+
+	/*$(function() {
 		$.getJSON('https://www.googleapis.com/youtube/v3/search', function(data) {
+			console.log(data);
 			myData = data.Search;
 			$.each(myData, function(index, value) {
 				console.log(value.Title);
 			})
 		})
-	});
+	});*/
 
 });
