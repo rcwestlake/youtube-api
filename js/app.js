cf1f4c30f33e92799784c179a40ecf728b1fcf1d
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-19 05:38:38 -0700

    code attempts for getting thumbnails

--- js/app.js
+++ js/app.js
@@ -1,27 +1,46 @@
 //my API key AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E
 
+var searchTerm = $('#query').val();
+
 $(document).ready(function() {
 	console.log('js check');
 
-
-
-	function onYouTubeApiLoad() {
-    	gapi.client.setApiKey('AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E');
-    }	
+	$(function() {
+		$('#search-term').submit('click', function() {
+			event.preventDefault();
+			var searchTerm = $('#query').val();
+			getRequest(searchTerm);
+				
+		})
+	})
 
     var getRequest = function(data) {
     	var params = {
     			part: 'snippet',
     			key: 'AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E',
-    			q: "basketball"
+    			q: searchTerm
     	};
     	var url = 'https://www.googleapis.com/youtube/v3/search';
 
     	$.getJSON(url, params, function(data) {
-    		console.log(data);
+    		myData = data.item.snippet.thumbnails.default;
+    		showResults(myData).show();	
+    		console.log(myData);
     	});
     }
 
+    var showResults = function(results) {
+    	var html = "";
+    	$.each(results, function(index,value) {
+    		html += '<div>' + value.thumbnails.default + '</div>';
+    	})
+    	$(".photos").html(html);
+    }
+
+
+    getRequest();
+
+    
 
 	/*$(function() {
 		$.getJSON('https://www.googleapis.com/youtube/v3/search', function(data) {
@@ -31,6 +50,8 @@ $(document).ready(function() {
 				console.log(value.Title);
 			})
 		})
-	});*/
+	});
+
+*/
 
 });
