4adc66e401c2a9a9c064879db51312ee3de077db
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-19 05:52:54 -0700

    js

--- js/app.js
+++ js/app.js
@@ -14,31 +14,31 @@ $(document).ready(function() {
 		})
 	})
 
-    var getRequest = function(data) {
+    var getRequest = function(searchTerm) {
     	var params = {
     			part: 'snippet',
     			key: 'AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E',
-    			q: searchTerm
+    			q: "basketball",
+    			
     	};
     	var url = 'https://www.googleapis.com/youtube/v3/search';
 
     	$.getJSON(url, params, function(data) {
-    		myData = data.item.snippet.thumbnails.default;
-    		showResults(myData).show();	
-    		console.log(myData);
+    		showResults(data);
+    		console.log(data);
     	});
     }
 
     var showResults = function(results) {
     	var html = "";
     	$.each(results, function(index,value) {
-    		html += '<div>' + value.thumbnails.default + '</div>';
+    		html += '<div>' + value.snippet.thumbnails.default + '</div>';
     	})
     	$(".photos").html(html);
     }
 
-
-    getRequest();
+    ///used this prior to calling getRequest in above function
+    //getRequest();
 
     
 
