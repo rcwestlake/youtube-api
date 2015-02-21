1b1a91ebda533eb56db312dbe68c4144fb717175
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-21 10:38:54 -0700

    deleted old js

--- js/app.js
+++ js/app.js
@@ -40,18 +40,4 @@ $(document).ready(function() {
     ///used this prior to calling getRequest in above function
     //getRequest();
 
-    
-
-	/*$(function() {
-		$.getJSON('https://www.googleapis.com/youtube/v3/search', function(data) {
-			console.log(data);
-			myData = data.Search;
-			$.each(myData, function(index, value) {
-				console.log(value.Title);
-			})
-		})
-	});
-
-*/
-
 });
