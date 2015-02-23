ecd02479e685c441a5daccf2be9ccaed2b5a6ecc
rcwestlake <ryanwestlake@MacBook-Pro.local>
2015-02-22 22:38:05 -0700

    added css and js to push photos in grid

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     			part: 'snippet',
     			key: 'AIzaSyC7ZQhFDobGaUbG2i5Q23a0ud6b6BwnT9E',
     			q: $('#query').val(),
-                maxResults: 25,        
+                maxResults: 50,        
     			
     	};
     	var url = 'https://www.googleapis.com/youtube/v3/search';
@@ -29,10 +29,11 @@ $(document).ready(function() {
     var showResults = function(results) {
     	var html = "";
     	$.each(results.items, function(index,item) {
-    		html += '<div><img src="' + item.snippet.thumbnails.default.url + '"></div>';
+    		html += '<div id="search-results"><img src="' + item.snippet.thumbnails.default.url + '"></div>';
     	})
     	$(".photos").html(html);
     }
+    
 
 
     ///used this prior to calling getRequest in above function
